77b81877221b0cd9a60de09d1b2837bb28aec2a4
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2017-09-22 09:40:46 +0200

    Towards a better Alt-Svc scrapper

--- scripts/alt_svc_scrapper.py
+++ scripts/alt_svc_scrapper.py
@@ -1,21 +1,30 @@
 import sys
+from http.client import HTTPException
 from ssl import CertificateError
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen, Request
 
+
+def scrape_domain(protocol, domain, user_agent='Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'):
+    url = '%s://%s' % (protocol, domain)
+    try:
+        request = Request(url, headers={'User-Agent': user_agent})
+        with urlopen(request, timeout=5) as response:
+            return url, response.info().get('Alt-Svc')
+    except HTTPError as e:
+        return url, e.headers.get('Alt-Svc')
+    except (URLError, CertificateError, HTTPException):
+        if protocol == 'https':
+            return scrape_domain('http', domain)
+    except OSError as e:
+        print(url, e)
+    return url, None
+
 if __name__ == "__main__":
-    url_list_filename = sys.argv[1]
-    with open(url_list_filename) as url_list_file:
-        url_list = url_list_file.read().strip()
-    for url in url_list.splitlines():
-        print(url, end='\t')
-        try:
-            request = Request(url, headers={'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0'})
-            response_headers = urlopen(url, timeout=5).info()
-            if 'Alt-Svc' in response_headers:
-                print('Alt-Svc: %s' % urlopen(url).info()['Alt-Svc'], end='')
-        except HTTPError as e:
-            print(e, end='')
-        except (OSError, CertificateError) as e:
-            print(e, end='')
-        print()
+    domain_list_filename = sys.argv[1]
+    with open(domain_list_filename) as domain_list_file:
+        domain_list = domain_list_file.read().strip()
+    for domain in domain_list.splitlines():
+        url, header_value = scrape_domain('https', domain)
+        print(url, header_value)
+

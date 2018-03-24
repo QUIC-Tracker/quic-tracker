7f5d7aef71616136d0a8a0fc33d27d0d325f1348
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-03-24 11:30:42 +0100

    Updates http_get_on_uni_stream to stop expecting HTTP response but only checking for abnormal response

--- scenarii/http_get_on_uni_stream.go
+++ scenarii/http_get_on_uni_stream.go
@@ -23,11 +23,12 @@ import (
 )
 
 const (
-	GS2_TLSHandshakeFailed               = 1
-	GS2_TooLowStreamIdUniToSendRequest   = 2
-	GS2_ReceivedDataOnStream2            = 3
-	GS2_ReceivedDataOnUnauthorizedStream = 4
+	GS2_TLSHandshakeFailed                    = 1
+	GS2_TooLowStreamIdUniToSendRequest        = 2
+	GS2_ReceivedDataOnStream2                 = 3
+	GS2_ReceivedDataOnUnauthorizedStream      = 4
 	GS2_AnswersToARequestOnAForbiddenStreamID = 5
+	GS2_DidNotCloseTheConnection              = 6
 )
 
 type GetOnStream2Scenario struct {
@@ -39,51 +40,34 @@ func NewGetOnStream2Scenario() *GetOnStream2Scenario {
 }
 
 func (s *GetOnStream2Scenario) Run(conn *m.Connection, trace *m.Trace, debug bool) {
-
-	errors := make(map[uint]bool)
-	var errorMessages []string
-
 	conn.Streams[3] = new(m.Stream)
 
 	conn.TLSTPHandler.MaxStreamIdBidi = 1
 	conn.TLSTPHandler.MaxStreamIdUni = 3
 	if err := CompleteHandshake(conn); err != nil {
-		errors[GS2_TLSHandshakeFailed] = true
 		trace.MarkError(GS2_TLSHandshakeFailed, err.Error())
 		return
 	}
 
 	if conn.TLSTPHandler.ReceivedParameters.MaxStreamIdUni < 2 {
-		trace.ErrorCode = GS2_TooLowStreamIdUniToSendRequest
-		trace.Results["error"] = fmt.Sprintf("the remote initial_max_stream_id_uni is %d", conn.TLSTPHandler.ReceivedParameters.MaxStreamIdUni)
 		trace.Results["received_transport_parameters"] = conn.TLSTPHandler.ReceivedParameters.ToJSON
 	}
 
 	conn.SendHTTPGETRequest("/index.html", 2)
 
+outerLoop:
 	for i := 0 ; i < 50 ; i++ {
 		readPacket, err, _ := conn.ReadNextPacket()
 		if err != nil {
 			switch e := err.(type) {
 			case *net.OpError:
 				// the peer timed out without closing the connection
-				if e.Timeout() {
-					if false {
-						// FIXME: accurate timeout computation
-						trace.ErrorCode = SGW_DidNotCloseTheConnection
-						errors[SGW_DidNotCloseTheConnection] = true
-						message := fmt.Sprintf("the peer did not close the connection after waiting %d seconds", conn.TLSTPHandler.ReceivedParameters.IdleTimeout)
-						trace.Results["error"] = message
-						errorMessages = append(errorMessages, message)
-					} else {
-					 	trace.ErrorCode = 0
-					}
-				} else {
-					trace.ErrorCode = SGW_UnknownError
-					errors[SGW_UnknownError] = true
-					trace.Results["error"] = e.Error()
-					errorMessages = append(errorMessages, e.Error())
+				if e.Timeout() && conn.TLSTPHandler.ReceivedParameters.MaxStreamIdUni < 2 {
+					trace.ErrorCode = GS2_DidNotCloseTheConnection
+					trace.Results["error"] = fmt.Sprintf("the peer did not close the connection after waiting %d seconds", conn.TLSTPHandler.ReceivedParameters.IdleTimeout)
 				}
+			default:
+				trace.Results["error"] = err.Error()
 			}
 			return
 		}
@@ -95,16 +79,19 @@ func (s *GetOnStream2Scenario) Run(conn *m.Connection, trace *m.Trace, debug boo
 				case *m.StreamFrame:
 					if f2.StreamId == 2 {
 						trace.MarkError(GS2_ReceivedDataOnStream2, "")
-						return
+						break outerLoop
 					} else if f2.StreamId > 3 {
 						trace.MarkError(GS2_ReceivedDataOnUnauthorizedStream, "")
 					} else if f2.StreamId == 3 && conn.TLSTPHandler.ReceivedParameters.MaxStreamIdUni < 2 {
 						// they answered us even if we sent a get request on a Stream ID above their initial_max_stream_id_uni
-						trace.MarkError(GS2_AnswersToARequestOnAForbiddenStreamID, "")
-						return
+						// trace.MarkError(GS2_AnswersToARequestOnAForbiddenStreamID, "")
+						// Let's be more liberal about this case until the HTTP mapping is adopted in an implementation draft
 					}
 				case *m.ConnectionCloseFrame:
-					return
+					if trace.ErrorCode == GS2_TooLowStreamIdUniToSendRequest {
+						trace.ErrorCode = 0
+					}
+					break outerLoop
 				}
 			}
 			if pp.ShouldBeAcknowledged() {
@@ -120,4 +107,6 @@ func (s *GetOnStream2Scenario) Run(conn *m.Connection, trace *m.Trace, debug boo
 		}
 
 	}
+
+	conn.CloseConnection(false, 0, "")
 }

8af6ad992452040254369b2b48e738d92e3b4773
Maxime Piraux <maxime.piraux@student.uclouvain.be>
2018-08-10 09:36:30 +0200

    scenario_runner.go is now just a scenario runner

--- bin/scenario/scenario_runner.go
+++ bin/scenario/scenario_runner.go
@@ -18,115 +18,68 @@ package main
 
 import (
 	"os"
-	"bufio"
 	m "github.com/mpiraux/master-thesis"
 	s "github.com/mpiraux/master-thesis/scenarii"
+	"flag"
+	"strings"
 	"time"
 	"encoding/json"
-	"strings"
-	"flag"
-	"log"
-	"net/http"
-	_ "net/http/pprof"
 )
 
-
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
-	hostFilename := flag.String("hosts", "", "The file containing the hosts to test.")
-	scenarioName := flag.String("scenario", "", "The particular scenario to test. Tests all of them if not set.")
-	outputFile := flag.String("output", "", "The file to write the output to. Output to stdin if not set.")
-	netInterface := flag.String("interface", "", "The interface to listen to when capturing pcap")
+	host := flag.String("host", "", "The host endpoint to run the test against.")
+	url := flag.String("url", "/index.html", "The URL to request when performing tests that needs data to be sent.")
+	scenarioName := flag.String("scenario", "", "The particular scenario to run.")
+	outputFile := flag.String("output", "", "The file to write the output to. Output to stdout if not set.")
+	debug := flag.Bool("debug", false, "Enables debugging information to be printed.")
+	netInterface := flag.String("interface", "", "The interface to listen to when capturing pcap.")
 	flag.Parse()
 
-	file, err := os.Open(*hostFilename)
-	if err != nil {
-		panic(err)
+	if *host == "" || *url == "" || *scenarioName == "" {
+		println("Parameters hostname, url and scenario are required")
+		os.Exit(-1)
 	}
-	defer file.Close()
 
-	debugSwitch, ok := os.LookupEnv("SCENARIO_RUNNER_DEBUG")
-	debug := ok && debugSwitch == "1"
-
-	scenarii := [...]s.Scenario{
-		s.NewZeroRTTScenario(),
-		s.NewConnectionMigrationScenario(),
-		s.NewUnsupportedTLSVersionScenario(),
-		s.NewStreamOpeningReorderingScenario(),
-		s.NewMultiStreamScenario(),
-		s.NewNewConnectionIDScenario(),
-		s.NewVersionNegotiationScenario(),
-		s.NewHandshakeScenario(),
-		s.NewHandshakev6Scenario(),
-		s.NewTransportParameterScenario(),
-		s.NewAddressValidationScenario(),
-		s.NewPaddingScenario(),
-		s.NewFlowControlScenario(),
-		s.NewAckOnlyScenario(),
-		s.NewStopSendingOnReceiveStreamScenario(),
-		s.NewSimpleGetAndWaitScenario(),
-		s.NewGetOnStream2Scenario(),
+	scenario, ok := s.GetAllScenarii()[*scenarioName]
+	if !ok {
+		println("Unknown scenario name", scenario)
 	}
-	results := make([]m.Trace, 0, 0)
 
-	for _, scenario := range scenarii {
-		if *scenarioName != "" && scenario.Name() != *scenarioName {
-			continue
+	trace := m.NewTrace(scenario.Name(), scenario.Version(), *host)
+
+	conn, err := m.NewDefaultConnection(*host, strings.Split(*host, ":")[0], nil, scenario.IPv6())  // Raw IPv6 are not handled correctly
+
+	if err == nil {
+		pcap, err := m.StartPcapCapture(conn, *netInterface)
+
+		trace.AttachTo(conn)
+
+		start := time.Now()
+		scenario.Run(conn, trace, *url, *debug)
+		trace.Duration = uint64(time.Now().Sub(start).Seconds() * 1000)
+		ip := strings.Replace(conn.ConnectedIp().String(), "[", "", -1)
+		trace.Ip = ip[:strings.LastIndex(ip, ":")]
+		trace.StartedAt = start.Unix()
+
+		conn.Close()
+		trace.Complete(conn)
+		err = trace.AddPcap(pcap)
+		if err != nil {
+			trace.Results["pcap_error"] = err.Error()
 		}
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			args := strings.Split(scanner.Text(), "\t")
-			host, preferredUrl := args[0], args[1]
-			if debug {
-				print(scenario.Name(), " ", host)
-			}
-
-			trace := m.NewTrace(scenario.Name(), scenario.Version(), host)
-
-			conn, err := m.NewDefaultConnection(host, strings.Split(host, ":")[0], nil, scenario.IPv6())
-
-			if err == nil {
-				pcap, err := m.StartPcapCapture(conn, *netInterface)
-
-				trace.AttachTo(conn)
-
-				start := time.Now()
-				scenario.Run(conn, trace, preferredUrl, debug)
-				trace.Duration = uint64(time.Now().Sub(start).Seconds() * 1000)
-				ip := strings.Replace(conn.ConnectedIp().String(), "[", "", -1)
-				trace.Ip = ip[:strings.LastIndex(ip, ":")]
-				trace.StartedAt = start.Unix()
-
-				conn.Close()
-				trace.Complete(conn)
-				err = trace.AddPcap(pcap)
-				if err != nil {
-					trace.Results["pcap_error"] = err.Error()
-				}
-				trace.DecryptedPcap, err = m.DecryptPcap(trace)
-				if err != nil {
-					trace.Results["pcap_decrypt_error"] = err.Error()
-				}
-			} else {
-				trace.ErrorCode = 255
-				trace.Results["udp_error"] = err.Error()
-			}
-
-			results = append(results, *trace)
-			if debug {
-				println(" ", trace.ErrorCode)
-			}
+		trace.DecryptedPcap, err = m.DecryptPcap(trace)
+		if err != nil {
+			trace.Results["pcap_decrypt_error"] = err.Error()
 		}
-		file.Seek(0, 0)
+	} else {
+		trace.ErrorCode = 255
+		trace.Results["udp_error"] = err.Error()
 	}
 
-	out, _ := json.Marshal(results)
+	out, _ := json.Marshal(trace)
 	if *outputFile != "" {
 		os.Remove(*outputFile)
-		outFile, err := os.OpenFile(*outputFile, os.O_CREATE | os.O_WRONLY, 0755)
+		outFile, err := os.OpenFile(*outputFile, os.O_CREATE|os.O_WRONLY, 0755)
 		defer outFile.Close()
 		if err == nil {
 			outFile.Write(out)

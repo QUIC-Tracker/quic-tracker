a44a129b8971bb34d3291907ce46b7c5a24bd4df
Maxime Piraux <maxime.piraux@uclouvain.be>
2019-11-07 13:41:17 +0100

    Adds a test for zero-length client CID

--- scenarii/scenario.go
+++ scenarii/scenario.go
@@ -128,5 +128,6 @@ func GetAllScenarii() map[string]Scenario {
 		"spin_bit":                   NewSpinBitScenario(),
 		"server_flow_control":        NewServerFlowControlScenario(),
 		"connection_migration_v4_v6": NewConnectionMigrationv4v6Scenario(),
+		"zero_length_cid": 			  NewZeroLengthCID(),
 	}
 }
---
+++ scenarii/zero_length_cid.go
@@ -0,0 +1,33 @@
+package scenarii
+
+import qt "github.com/QUIC-Tracker/quic-tracker"
+
+const (
+	ZLCID_TLSHandshakeFailed = 1
+	ZLCID_RequestFailed = 2
+)
+
+type ZeroLengthCID struct {
+	AbstractScenario
+}
+
+func NewZeroLengthCID() *ZeroLengthCID {
+	return &ZeroLengthCID{AbstractScenario{name: "zero_length_cid", version: 1}}
+}
+
+func (s *ZeroLengthCID) Run(conn *qt.Connection, trace *qt.Trace, preferredPath string, debug bool) {
+	conn.SourceCID = nil
+	connAgents := s.CompleteHandshake(conn, trace, ZLCID_TLSHandshakeFailed)
+	if connAgents == nil {
+		return
+	}
+
+	connAgents.AddHTTPAgent().SendRequest(preferredPath, "GET", trace.Host, nil)
+
+	<-s.Timeout()
+
+	if !conn.Streams.Get(0).ReadClosed {
+		trace.ErrorCode = ZLCID_RequestFailed
+	}
+}
+
